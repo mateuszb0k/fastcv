0959243c7419072fb63637b411d2df131afeb3c1
adimac13 <s197932@student.pg.edu.pl>
2026-01-22 16:57:34 +0100

    Fixed setup

--- examples/rgb2gray.py
+++ examples/rgb2gray.py
@@ -6,6 +6,6 @@ img = cv2.imread("artifacts/test.jpg")
 img_tensor = torch.from_numpy(img).cuda()
 gray_tensor = fastcv.rgb2gray(img_tensor)
 gray_np = gray_tensor.squeeze(-1).cpu().numpy()
-cv2.imwrite("output_gray.jpg", gray_np)
+cv2.imwrite("output_gray.jpg", gray_np) 
 
 print("saved grayscale image.")
--- setup.py
+++ setup.py
@@ -15,8 +15,34 @@ setup(
                 "kernels/template_match.cu",
                 "kernels/module.cpp",
             ],
-            extra_compile_args={"cxx": ["-O2"]},
+            extra_compile_args={
+                "cxx": [
+                    "/O2",
+                    "/std:c++17",
+                    "/permissive-",
+                    "/DNOMINMAX"
+                ],
+                "nvcc": [
+                    "-O2",
+                    "--std=c++17",
+                    "-Xcompiler", "/permissive-",
+                    "-Xcompiler", "/DNOMINMAX",
+                    "-D__CUDA_NO_HALF_OPERATORS__",
+                    "-D__CUDA_NO_HALF_CONVERSIONS__",
+                    "-D__CUDA_NO_HALF2_OPERATORS__"
+                ]
+            },
         ),
     ],
-    cmdclass={"build_ext": BuildExtension},
+    cmdclass={"build_ext": BuildExtension.with_options(use_ninja=False)},
 )
+
+# To build the extension open x64 Native Tools Command Prompt for VS 2022
+# Enter the directory where setup.py is located
+# Paste these commands:
+# set DISTUTILS_USE_SDK=1
+# set CUDA_HOME= "Path where you keep CUDA toolkit e.g. ("C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6")
+# set USE_NINJA=0
+# set SETUPTOOLS_ENABLE_FEATURES="legacy-editable"
+# And finnaly run:
+# pip install -e . --no-build-isolation
