8971fc0bdde47c46fcda59c0117a9e665b50d8b6
mateuszb0k <mateuszblock@icloud.com>
2026-01-23 11:24:32 +0100

    using thrust to find the minimum

--- kernels/template_match.cu
+++ kernels/template_match.cu
@@ -1,9 +1,19 @@
 #include <torch/extension.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/transform.h>
+#include <thrust/universal_vector.h>
+#include <thrust/pair.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cuda_runtime.h>
 #include <stdio.h>
 #include "utils.cuh"
+//type aliases 
+//modern cpp requirements
+using DeviceVec = thrust::device_vector<float>;
+using HostVec = thrust::host_vector<float>;
+using UniversalVec = thrust::universal_vector<float>;
 
 __global__ void templateMatchKernel
 (
@@ -18,6 +28,17 @@ __global__ void templateMatchKernel
 	int pad_y = tpl_h / 2; 
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	//load template into shared memory for faster access
+	extern __shared__ float shared_tpl[];
+	int template_id = threadIdx.y * blockDim.x + threadIdx.x;
+	int template_size = tpl_w * tpl_h;
+	for (int i = template_id; i < template_size; i += blockDim.x * blockDim.y) {
+		shared_tpl[i] = templ[i];
+	}
+	__syncthreads();
+
+
 	// corelation for each pixel
 	if (x < res_w && y < res_h) {
 		float sum = 0.0f;
@@ -26,7 +47,7 @@ __global__ void templateMatchKernel
 				// if we flatten the image we get y*width + x so for correlation we offset by i and j thus
 				float img_val = input[(y + j) * img_w + x + i];
 				// get template value simply y_tpl*width_tpl + x_tpl 
-				float tpl_val = templ[j * tpl_w + i];
+				float tpl_val = shared_tpl[j * tpl_w + i];
 				float diff = img_val - tpl_val;
 				sum += diff * diff;//TM_SQ_DIFF
 			}
@@ -47,10 +68,14 @@ torch::Tensor template_match(torch::Tensor img, torch::Tensor templ) {
 	int tpl_w = templ.size(1);
 	int res_w = img_w - tpl_w + 1;
 	int res_h = img_h - tpl_h + 1;
+	int pad_x = tpl_w / 2;
+	int pad_y = tpl_h / 2;
 	auto result = torch::full_like(img,1000000.0f);
+	size_t shared_mem_size = tpl_w * tpl_h * sizeof(float);
+
 	dim3 dimBlock = getOptimalBlockDim(res_w, res_h);
 	dim3 dimGrid(cdiv(res_w, dimBlock.x), cdiv(res_h, dimBlock.y));
-	templateMatchKernel <<< dimGrid, dimBlock >>> (
+	templateMatchKernel <<< dimGrid, dimBlock, shared_mem_size >>> (
 		img.data_ptr<float>(),
 		templ.data_ptr<float>(),
 		result.data_ptr<float>(),
@@ -59,6 +84,36 @@ torch::Tensor template_match(torch::Tensor img, torch::Tensor templ) {
 
 		);
 	cudaDeviceSynchronize();
+	float* result_ptr = result.data_ptr<float>();
+	//pair of value and index
+	auto extract_value = [=] __device__(int idx) -> thrust::pair<float, int> {
+		int x = idx % res_w;
+		int y = idx / res_w;
+		int out_idx = (y + pad_y) * img_w + (x + pad_x);
+		return thrust::pair<float, int>(result_ptr[out_idx], out_idx);
+
+	};
+	//iterate over all result values
+	auto value_iter = thrust::make_transform_iterator(
+		thrust::counting_iterator<int>(0),
+		extract_value
+	);
+	//find minimum value and its index
+	auto min_result = thrust::reduce(
+		thrust::device,
+		value_iter,
+		value_iter + (res_w * res_h),
+		thrust::pair<float, int>(1e9f, -1),
+		[]__device__(thrust::pair<float, int> a, thrust::pair<float, int>b) {
+		return (a.first > b.first) ? b : a;
+	}
+	);
+	float min_value = min_result.first;
+	int min_index = min_result.second;
+	int min_x = min_index % img_w;
+	int min_y = min_index / img_w;
+	printf("SQ_DIFF min val:%f at (x = %d,y = %d)\n", min_value, min_x, min_y);
+
 	return result;
 
 }
